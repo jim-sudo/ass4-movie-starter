3093de7b3c807452f04feef5de9d2bfe9e9bf795
Majid Iqbal <majidqbl1@gmail.com>
2024-05-17 22:57:53 +0000

    made changes in movie

--- movie.h
+++ movie.h
@@ -1,7 +1,18 @@
 #ifndef MOVIE_H_
 #define MOVIE_H_
+#include <ostream>
+#include <vector>
+
+using namespace std;
+
 
 class Movie {
+    friend ostream &operator<<(ostream &out, const Movie &movie);
+
+public:
+
+private:
+
 
 };
 #endif
