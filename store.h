5db371f963ecce15a813243210609db3896d0877
jim-sudo <jimmyosburn@rocketmail.com>
2024-05-17 16:07:38 -0700

    store.h so far. Is incomplete

---
+++ store.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "movie.h"
+#include "HashTable.h"
+#include ""
+
+using namespace std;
+class Customer {
+    private:
+        std::vector<Movie> movies_;
+        HashTable customer_table_;
+    public: 
+        void ReadMovies()
+};
